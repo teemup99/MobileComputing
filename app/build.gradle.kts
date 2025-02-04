606052c82f66917446a0df85f0003928ce76b5bd
teemup99 <tpuro19@edu.oulu.fi>
2025-02-04 17:36:48 +0200

    add Homework 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -41,6 +42,22 @@ android {
 
 dependencies {
 
+    val nav_version = "2.8.5"
+    // Jetpack Compose integration
+    implementation(libs.androidx.navigation.compose)
+
+    // Views/Fragments integration
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+
+    // Feature module support for Fragments
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+
+    // Testing Navigation
+    androidTestImplementation(libs.androidx.navigation.testing)
+
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/harjoitus1/MainActivity.kt
+++ app/src/main/java/com/example/harjoitus1/MainActivity.kt
@@ -27,7 +27,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import android.content.res.Configuration
+import android.graphics.drawable.shapes.OvalShape
 import android.renderscript.Sampler
+import android.util.Log
+import android.widget.Space
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.clickable
@@ -37,15 +40,41 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.harjoitus1.ui.theme.Harjoitus1Theme
+import kotlinx.serialization.Serializable
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             Harjoitus1Theme {
-                Conversation(SampleData.conversationSample)
+                Box(modifier = Modifier.fillMaxSize().systemBarsPadding()) {
+                MainApp()
+                //Conversation(SampleData.conversationSample)
+                }
             }
         }
     }
@@ -53,8 +82,20 @@ class MainActivity : ComponentActivity() {
 
 data class Message(val author: String, val body: String)
 
+@Serializable
+object MessageCard
+
+@Serializable
+object ProfileData
+
+@Serializable
+object Conversation2
+
 @Composable
-fun MessageCard(msg: Message) {
+fun MessageCardScreen(
+    msg: Message,
+    onNavigateToProfileData: () -> Unit,
+    ) {
     // Add padding around message
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
@@ -111,10 +152,136 @@ fun MessageCard(msg: Message) {
 }
 
 @Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
+fun ProfileDataScreen(
+    msg: Message,
+    onNavigateToConversation2: () -> Unit) {
+    // Main Column to hold top bar and profile
+    Column(modifier = Modifier.fillMaxSize()) {
+        // Top Bar with button
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp), //top bar height
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Button(
+                    onClick = { onNavigateToConversation2() },
+                    border = BorderStroke(1.5.dp, MaterialTheme.colorScheme.surface)
+                ) {
+                    Text("Back")
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.cirn),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = msg.author,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Phone: 040 XXXXXXX",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+    }
+}
+
+@Composable
+fun ConversationScreen(
+    messages: List<Message>,
+    onNavigateToProfileData: () -> Unit
+) {
+    // Main column to hold top bar and conversation
+    Column(modifier = Modifier.fillMaxSize()) {
+        // Top Bar with button
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp), //top bar height
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+            ) {
+                // Button inside top bar
+                Button(
+                    onClick = { onNavigateToProfileData() },
+                    border = BorderStroke(1.5.dp, MaterialTheme.colorScheme.surface)
+                ) {
+                    Text("View Profile")
+                }
+            }
+        }
+        // LazyColumn with messages
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 8.dp) // Padding to avoid content being hidden behind the top bar
+        ) {
+            items(messages) { message ->
+                MessageCardScreen(message, onNavigateToProfileData = { })
+            }
+        }
+    }
+}
+
+@Composable
+fun MainApp() {
+    // Init navController
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "conversation2") {
+        composable("conversation2") {backStackEntry ->
+            val currentDestination: Conversation2 = backStackEntry.toRoute()
+            ConversationScreen(
+                SampleData.conversationSample,
+                onNavigateToProfileData = {
+                    navController.navigate("profileData")
+                }
+            )
+        }
+    /*    composable("messageCard") {
+            MessageCardScreen(
+                msg = Message("Cirno", "Hey, take a look at Jetpack Compose"),
+                onNavigateToProfileData = {
+                    navController.navigate(
+                        "profileData"
+                    )
+                }
+            )
+        }*/
+        composable("profileData") {
+            ProfileDataScreen(
+                msg = Message("Cirno", "Hello there!"),
+                onNavigateToConversation2 = {
+                    navController.navigate("conversation2")
+                }
+            )
         }
     }
 }
@@ -129,10 +296,18 @@ fun Conversation(messages: List<Message>) {
 fun MessageCardPreview() {
     Harjoitus1Theme {
         Surface {
-            MessageCard(
-                msg = Message("Cirno", "Hey, take a look at Jetpack Compose")
-            )
+            MessageCardScreen(
+                msg = Message("Cirno", "Hey, take a look at Jetpack Compose"), {  })
         }
+    }
+}
+
+@Preview
+@Composable
+fun ProfileDataPreview() {
+    Harjoitus1Theme {
+        ProfileDataScreen(
+            msg = Message("Cirno", "Hello there"), {})
     }
 }
 
@@ -140,6 +315,6 @@ fun MessageCardPreview() {
 @Composable
 fun ConversationPreview() {
     Harjoitus1Theme {
-        Conversation(SampleData.conversationSample)
+        ConversationScreen(SampleData.conversationSample, {})
     }
 }
